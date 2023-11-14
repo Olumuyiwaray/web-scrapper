aaf4b2e7216a6f9e84addb73cc74cfc9189077e7
Samuel Jimoh <jimohsamolumuyiwa@gmail.com>
2023-11-14 11:48:28 +0100

    Added url checking functionality to check for valid urls

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const bodyParser = require('body-parser');
 
 const axios = require('axios');
 
+const isUrl = require('is-url');
+
 const port = process.env.port || 3000;
 
 const cheerio = require('cheerio');
@@ -16,7 +18,8 @@ const app = express();
 // Configure express middlewares and template engines
 
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static('public'));
 
 app.set('views', './views');
 
@@ -33,25 +36,35 @@ app.get('/', async (req, res) => {
 // post request to handle data coming from frontend
 app.post('/scrape', async (req, res) => {
     const states = [];
-    try {
-        let url = req.body.url;
-        const selector = req.body.selector;
-        console.log(selector)
-        let data = await axios.get(url);
-       const $ =  cheerio.load(data.data);
-            $(selector).each((index, element) => {
-                /* Javascript string  methods replace and trim
-                are used in the code snippet below to replace
-                any line breaks with empty strings and remove
-                any whitespaces respectively */
-                const newData = $(element).text().replace(/\n/g, '').trim();
-                states.push(newData)
-            })
-           res.status(200).json(states)
-             
-    } catch (error) {
-        console.log(error);
+    let url = req.body.input;
+    console.log(url)
+
+    if (isUrl(url)) {
+        try {
+        
+        
+            const selector = req.body.selector;
+            console.log(selector)
+            let data = await axios.get(url);
+           const $ =  cheerio.load(data.data);
+                $(selector).each((index, element) => {
+                    /* Javascript string  methods replace and trim
+                    are used in the code snippet below to replace
+                    any line breaks with empty strings and remove
+                    any whitespaces respectively */
+                    const newData = $(element).text().replace(/\n/g, '').trim();
+                    states.push(newData)
+                })
+               res.status(200).json(states)
+                 
+        } catch (error) {
+            console.log(error);
+        }
+    } else {
+        res.status(400).json( {error: "Please input a valid url"} );
     }
+
+  
 });
 
 
